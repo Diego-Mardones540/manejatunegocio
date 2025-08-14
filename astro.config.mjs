58273f4f940e198603747648d562d4bc8d57d862
Mardones567 <demardones@uc.cl>
2025-08-14 02:30:12 -0400

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  base : '/manejatunegocio',
-  site:'https://github.com/Diego-Mardones540/manejatunegocio.io'
+  base: '/manejatunegocio',
+  site: 'https://Diego-Mardones540.github.io/manejatunegocio/'
 });
